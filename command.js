f0b3d7b3b143051c6176fd07515f583ac5cee761
wips <33335084+wipsdafox@users.noreply.github.com>
2017-12-14 09:12:52 -0700

    Update command.js

--- command.js
+++ command.js
@@ -1,31 +1,134 @@
 console.log("Copyright 2017 Johnny Stene, Some Rights Reserved.");
 console.log("Hint: /command.js");
 
-var message;
+var Konami = function (callback) {
+	var konami = {
+		addEvent: function (obj, type, fn, ref_obj) {
+			if (obj.addEventListener)
+				obj.addEventListener(type, fn, false);
+			else if (obj.attachEvent) {
+				// IE
+				obj["e" + type + fn] = fn;
+				obj[type + fn] = function () {
+					obj["e" + type + fn](window.event, ref_obj);
+				}
+				obj.attachEvent("on" + type, obj[type + fn]);
+			}
+		},
+		removeEvent: function (obj, eventName, eventCallback) {
+			if (obj.removeEventListener) {
+				obj.removeEventListener(eventName, eventCallback);
+			} else if (obj.attachEvent) {
+				obj.detachEvent(eventName);
+			}
+		},
+		input: "",
+		pattern: "38384040373937396665",
+		keydownHandler: function (e, ref_obj) {
+			if (ref_obj) {
+				konami = ref_obj;
+			} // IE
+			konami.input += e ? e.keyCode : event.keyCode;
+			if (konami.input.length > konami.pattern.length) {
+				konami.input = konami.input.substr((konami.input.length - konami.pattern.length));
+			}
+			if (konami.input === konami.pattern) {
+				konami.code(this._currentlink);
+				konami.input = '';
+				e.preventDefault();
+				return false;
+			}
+		},
+		load: function (link) {
+			this.addEvent(document, "keydown", this.keydownHandler, this);
+			this.iphone.load(link);
+		},
+		unload: function () {
+			this.removeEvent(document, 'keydown', this.keydownHandler);
+			this.iphone.unload();
+		},
+		code: function (link) {
+			window.location = link
+		},
+		iphone: {
+			start_x: 0,
+			start_y: 0,
+			stop_x: 0,
+			stop_y: 0,
+			tap: false,
+			capture: false,
+			orig_keys: "",
+			keys: ["UP", "UP", "DOWN", "DOWN", "LEFT", "RIGHT", "LEFT", "RIGHT", "TAP", "TAP"],
+			input: [],
+			code: function (link) {
+				konami.code(link);
+			},
+			touchmoveHandler: function (e) {
+				if (e.touches.length === 1 && konami.iphone.capture === true) {
+					var touch = e.touches[0];
+					konami.iphone.stop_x = touch.pageX;
+					konami.iphone.stop_y = touch.pageY;
+					konami.iphone.tap = false;
+					konami.iphone.capture = false;
+					konami.iphone.check_direction();
+				}
+			},
+			toucheendHandler: function () {
+				if (konami.iphone.tap === true) {
+					konami.iphone.check_direction(this._currentLink);
+				}
+			},
+			touchstartHandler: function (e) {
+				konami.iphone.start_x = e.changedTouches[0].pageX;
+				konami.iphone.start_y = e.changedTouches[0].pageY;
+				konami.iphone.tap = true;
+				konami.iphone.capture = true;
+			},
+			load: function (link) {
+				this.orig_keys = this.keys;
+				konami.addEvent(document, "touchmove", this.touchmoveHandler);
+				konami.addEvent(document, "touchend", this.toucheendHandler, false);
+				konami.addEvent(document, "touchstart", this.touchstartHandler);
+			},
+			unload: function () {
+				konami.removeEvent(document, 'touchmove', this.touchmoveHandler);
+				konami.removeEvent(document, 'touchend', this.toucheendHandler);
+				konami.removeEvent(document, 'touchstart', this.touchstartHandler);
+			},
+			check_direction: function () {
+				x_magnitude = Math.abs(this.start_x - this.stop_x);
+				y_magnitude = Math.abs(this.start_y - this.stop_y);
+				x = ((this.start_x - this.stop_x) < 0) ? "RIGHT" : "LEFT";
+				y = ((this.start_y - this.stop_y) < 0) ? "DOWN" : "UP";
+				result = (x_magnitude > y_magnitude) ? x : y;
+				result = (this.tap === true) ? "TAP" : result;
+				return result;
+			}
+		}
+	}
 
-function superHiddenFunction()
-{
-  eval(atob("ZXZhbChhdG9iKCJaWFpoYkNoaGRHOWlLQ0phV0Zwb1lrTm9hR1JIT1dsTFEwcGhVbnBzY1ZwR1kzaGlSMHAxVlZoV1lVMXNXWGRWYkdRMFlrZEtXRlp1Vm10U1ZXOHhWVEZrVW1Jd2JIUlVibHBwVFc1a2NGTXhUVEZqUjBwMFRsZDRhbUV5YUZaV1JsWXpXakZDVkZGWGJGWlRSWEF5VjJwT1MyRkhTbGhOVjNoaFVUQktjRnBXVGtOVE1rbDVZVWhXYVdKdGRHNVdWRTVUWWtkS2RGWllWa3BTVjNSdVdWVmpOV1F4Y0ZSUmFsWnBUVEZXYmxsVlpFZE5iSEJVVVcxb1NsSjZWbmRYVkVwV1dqRndTRkpxVmsxaFZXc3pTV2xyY0U5M1BUMGlLU2s3IikpOw=="));
-  return atob("SGVsbG8gdHJhdmVsZXIhIE15IG5hbWUgaXMgSm9obm55IFN0ZW5lLCBhbmQgSSBhbSB0aGUgZGV2ZWxvcGVyIG9mIHRoaXMgd2ViIGFwcC4gSSBob3BlIHlvdSBoYXZlIGEgZ29vZCBkYXksIGFuZCBiZSBzdXJlIHRvIGNoZWNrIG91dCBteSBHaXRIdWIgYXQgZ2l0aHViLmNvbS93aXBzZGFmb3gh");
+	typeof callback === "string" && konami.load(callback);
+	if (typeof callback ==
+      
+      = "function") {
+		konami.code = callback;
+		konami.load();
+	}
+
+	return konami;
+};
+
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+		module.exports = Konami;
+} else {
+		if (typeof define === 'function' && define.amd) {
+				define([], function() {
+						return Konami;
+				});
+		} else {
+				window.Konami = Konami;
+		}
 }
 
-function secret()
-{
-  if(message == undefined)
-  {
-    randomNum = Math.floor((Math.random() * 100) + 1); //1 in 10 chance
-    console.log(`randomNum == ${randomNum}`);
-    if(randomNum == 5)
-    {
-      message = atob("SGVsbG8hIFBsZWFzZSB1c2UgdGhlIGphdmFzY3JpcHQgY29uc29sZSBvbiB5b3VyIGJyb3dzZXIgKHVzdWFsbHkgQ3RybCtTaGlmdCtLKSB0byBydW4gdGhlIGZ1bmN0aW9uICJzdXBlckhpZGRlbkZ1bmN0aW9uKCk7Ii4gVGhpcyBpcyBhIHRvcCBzZWNyZXQgbWVzc2FnZSwgcGxlYXNlIGRvIG5vdCB0ZWxsIGFueW9uZSEgSGF2ZSBhIG5pY2UgZGF5Lg==");
-      document.getElementById("title").innerHTML = message;
-    }
-  }
-  else
-  {
-    message = atob("SGVsbG8hIFBsZWFzZSB1c2UgdGhlIGphdmFzY3JpcHQgY29uc29sZSBvbiB5b3VyIGJyb3dzZXIgKHVzdWFsbHkgQ3RybCtTaGlmdCtLKSB0byBydW4gdGhlIGZ1bmN0aW9uICJzdXBlckhpZGRlbkZ1bmN0aW9uKCk7Ii4gVGhpcyBpcyBhIHRvcCBzZWNyZXQgbWVzc2FnZSwgcGxlYXNlIGRvIG5vdCB0ZWxsIGFueW9uZSEgSGF2ZSBhIG5pY2UgZGF5Lg==");
-    document.getElementById("title").innerHTML = message;
-  }
-}
-
-secret();
+var easter_egg = new Konami('wipsdafox.github.io');
