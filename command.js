7730c9355deb46f6c79548c2d23a3d9e3132f1a7
wips <33335084+wipsdafox@users.noreply.github.com>
2017-12-18 14:55:52 -0700

    Update command.js

--- command.js
+++ command.js
@@ -1,133 +1,29 @@
-console.log("Hint: /command.js");
+var complete;
 
-var Konami = function (callback) {
-	var konami = {
-		addEvent: function (obj, type, fn, ref_obj) {
-			if (obj.addEventListener)
-				obj.addEventListener(type, fn, false);
-			else if (obj.attachEvent) {
-				// IE
-				obj["e" + type + fn] = fn;
-				obj[type + fn] = function () {
-					obj["e" + type + fn](window.event, ref_obj);
-				}
-				obj.attachEvent("on" + type, obj[type + fn]);
-			}
-		},
-		removeEvent: function (obj, eventName, eventCallback) {
-			if (obj.removeEventListener) {
-				obj.removeEventListener(eventName, eventCallback);
-			} else if (obj.attachEvent) {
-				obj.detachEvent(eventName);
-			}
-		},
-		input: "",
-		pattern: "38384040373937396665",
-		keydownHandler: function (e, ref_obj) {
-			if (ref_obj) {
-				konami = ref_obj;
-			} // IE
-			konami.input += e ? e.keyCode : event.keyCode;
-			if (konami.input.length > konami.pattern.length) {
-				konami.input = konami.input.substr((konami.input.length - konami.pattern.length));
-			}
-			if (konami.input === konami.pattern) {
-				konami.code(this._currentlink);
-				konami.input = '';
-				e.preventDefault();
-				return false;
-			}
-		},
-		load: function (link) {
-			this.addEvent(document, "keydown", this.keydownHandler, this);
-			this.iphone.load(link);
-		},
-		unload: function () {
-			this.removeEvent(document, 'keydown', this.keydownHandler);
-			this.iphone.unload();
-		},
-		code: function (link) {
-			window.location = link
-		},
-		iphone: {
-			start_x: 0,
-			start_y: 0,
-			stop_x: 0,
-			stop_y: 0,
-			tap: false,
-			capture: false,
-			orig_keys: "",
-			keys: ["UP", "UP", "DOWN", "DOWN", "LEFT", "RIGHT", "LEFT", "RIGHT", "TAP", "TAP"],
-			input: [],
-			code: function (link) {
-				konami.code(link);
-			},
-			touchmoveHandler: function (e) {
-				if (e.touches.length === 1 && konami.iphone.capture === true) {
-					var touch = e.touches[0];
-					konami.iphone.stop_x = touch.pageX;
-					konami.iphone.stop_y = touch.pageY;
-					konami.iphone.tap = false;
-					konami.iphone.capture = false;
-					konami.iphone.check_direction();
-				}
-			},
-			toucheendHandler: function () {
-				if (konami.iphone.tap === true) {
-					konami.iphone.check_direction(this._currentLink);
-				}
-			},
-			touchstartHandler: function (e) {
-				konami.iphone.start_x = e.changedTouches[0].pageX;
-				konami.iphone.start_y = e.changedTouches[0].pageY;
-				konami.iphone.tap = true;
-				konami.iphone.capture = true;
-			},
-			load: function (link) {
-				this.orig_keys = this.keys;
-				konami.addEvent(document, "touchmove", this.touchmoveHandler);
-				konami.addEvent(document, "touchend", this.toucheendHandler, false);
-				konami.addEvent(document, "touchstart", this.touchstartHandler);
-			},
-			unload: function () {
-				konami.removeEvent(document, 'touchmove', this.touchmoveHandler);
-				konami.removeEvent(document, 'touchend', this.toucheendHandler);
-				konami.removeEvent(document, 'touchstart', this.touchstartHandler);
-			},
-			check_direction: function () {
-				x_magnitude = Math.abs(this.start_x - this.stop_x);
-				y_magnitude = Math.abs(this.start_y - this.stop_y);
-				x = ((this.start_x - this.stop_x) < 0) ? "RIGHT" : "LEFT";
-				y = ((this.start_y - this.stop_y) < 0) ? "DOWN" : "UP";
-				result = (x_magnitude > y_magnitude) ? x : y;
-				result = (this.tap === true) ? "TAP" : result;
-				return result;
-			}
-		}
-	}
-
-	typeof callback === "string" && konami.load(callback);
-	if (typeof callback ==
-      
-      = "function") {
-		konami.code = callback;
-		konami.load();
-	}
-
-	return konami;
-};
-
-
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-		module.exports = Konami;
-} else {
-		if (typeof define === 'function' && define.amd) {
-				define([], function() {
-						return Konami;
-				});
-		} else {
-				window.Konami = Konami;
-		}
+function hash(thingy)
+{
+	thing = new ArrayBuffer(thingy.length());
+	thing = thingy;
+	var hash = crypto.subtle.digest("SHA-512", thing);
+	return hash;
 }
 
-var easter_egg = new Konami(function() { alert('Copyright 2017 Johnny Stene Some Rights Reserved')});
+function easterEgg1(text)
+{
+	complete = text;
+	if(hash(text) == "7e8d5beb64358fff0f9dfc64ea4c54ad21b6fae3cffee24da5e7d93f6d4da4d3ac8963a0c3b91c97066b7982f0ab386503e5402e356b0dc7e988092178ddc7c7")
+	{
+		console.log("Picked lock 1/2.");
+		console.log("Hint: Author");
+	}
+}
+
+function easterEgg2(text)
+{
+	if(!hash(complete) == "7e8d5beb64358fff0f9dfc64ea4c54ad21b6fae3cffee24da5e7d93f6d4da4d3ac8963a0c3b91c97066b7982f0ab386503e5402e356b0dc7e988092178ddc7c7") return;
+	if(hash(text) == "baf7e7ad0ee64b5f24d23c54533023c3a6fe44daa1a3274176569adcf9c40eddbaa617b69b56ed5ae96a35ca8826895fc30faf66d86b4803738dc33f046214d5")
+	{
+		console.log("Picked lock 2/2.");
+		console.log(btoa("aHR0cHM6Ly9naXRodWIuY29tL3dpcHNkYWZveA=="));
+	}
+}
