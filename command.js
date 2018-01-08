8076aa788f3fb183687d5c8dd543ec5be64d0720
wips <33335084+wipsdafox@users.noreply.github.com>
2018-01-08 09:04:52 -0700

    Removed stupid easter egg

--- command.js
+++ command.js
@@ -1,30 +1,3 @@
-var complete;
+var copyrightYear = "Copyright " + (new Date()).getFullYear().toString() + " Johnny Stene. All Rights Reserved.";
 
-function hash(thingy)
-{
-	thing = new ArrayBuffer(thingy.length());
-	thing = thingy;
-	var hash = crypto.subtle.digest("SHA-512", thing);
-	return hash;
-}
-
-function easterEgg1(text)
-{
-	complete = text;
-	if(hash(text) == "7e8d5beb64358fff0f9dfc64ea4c54ad21b6fae3cffee24da5e7d93f6d4da4d3ac8963a0c3b91c97066b7982f0ab386503e5402e356b0dc7e988092178ddc7c7")
-	{
-		console.log("Picked lock 1/2.");
-		console.log("Hint: Author");
-	}
-}
-
-function easterEgg2(text)
-{
-	if(!hash(complete) == "7e8d5beb64358fff0f9dfc64ea4c54ad21b6fae3cffee24da5e7d93f6d4da4d3ac8963a0c3b91c97066b7982f0ab386503e5402e356b0dc7e988092178ddc7c7") return;
-	if(hash(text) == "baf7e7ad0ee64b5f24d23c54533023c3a6fe44daa1a3274176569adcf9c40eddbaa617b69b56ed5ae96a35ca8826895fc30faf66d86b4803738dc33f046214d5")
-	{
-		console.log("Picked lock 2/2.");
-		console.log(btoa("RtaWj1lUFwuDmSU0Zsq3iMjh9v/wvK8TV16wp9QnmnE=));
-		console.log("Hint:  aesencryption.net");
-	}
-}
+document.getElementById("copyright").innerHTML = copyrightYear;
